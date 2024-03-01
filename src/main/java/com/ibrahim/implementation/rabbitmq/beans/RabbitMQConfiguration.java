eca92b1e098c9ebcb91308e501382e1c88eb7889
ibnrahkm <mdibrahimk935@gmail.com>
2024-03-01 17:42:53 +0600

    somethings are changed

--- src/main/java/com/ibrahim/implementation/rabbitmq/beans/RabbitMQConfiguration.java
+++ src/main/java/com/ibrahim/implementation/rabbitmq/beans/RabbitMQConfiguration.java
@@ -5,6 +5,8 @@ import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.RoutingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.SimpleRoutingConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.beans.factory.annotation.Value;
--- src/main/java/com/ibrahim/implementation/rabbitmq/service/impl/QueueServiceImpl.java
+++ src/main/java/com/ibrahim/implementation/rabbitmq/service/impl/QueueServiceImpl.java
@@ -4,8 +4,11 @@ import com.ibrahim.implementation.rabbitmq.service.QueueService;
 import com.rabbitmq.client.AMQP;
 import com.rabbitmq.client.Channel;
 import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.Connection;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +20,9 @@ public class QueueServiceImpl implements QueueService {
     @Autowired
     CachingConnectionFactory connectionFactory;
 
+    @Autowired
+    RabbitTemplate rabbitTemplate;
+
     @Override
     public String createQueue(String name) {
         try {
@@ -24,6 +30,7 @@ public class QueueServiceImpl implements QueueService {
             Channel channel = connection.createChannel(true);
             AMQP.Queue.DeclareOk declareOk = channel.queueDeclare(name, true, false, false, null);
             channel.close();
+            BindingBuilder.bind(new Queue(name, true)).to(new DirectExchange(name, true, false)).with(name);
             return declareOk.getQueue();
         } catch (Exception e) {
             e.printStackTrace();
